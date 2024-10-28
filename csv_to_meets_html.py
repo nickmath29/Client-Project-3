70bb7ac67e2a085a44c469da9d82e6d041653275
nickmath <113207847+nickmath29@users.noreply.github.com>
2024-10-27 22:09:59 -0400

    done

--- csv_to_meets_html.py
+++ csv_to_meets_html.py
@@ -85,7 +85,7 @@ def csv_to_html(csv_filename, output_folder):
    <a href = "#main">Skip to Main Content</a>
    <nav>
      <ul>
-        <li><a href="../index.html">Home Page</a></li>
+        <li><a href="../">Home Page</a></li>
         <li><a href="#summary">Summary</a></li>
         <li><a href="#team-results">Team Results</a></li>
         <li><a href="#individual-results">Individual Results</a></li>
